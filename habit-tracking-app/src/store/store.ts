f29c0d5029bf6d8022efc2a8352be92c464b89f9
RajithaKWG <rkrishan894@gmail.com>
2024-10-22 13:47:39 +0530

    implemet store and add explaination comments

--- habit-tracking-app/src/store/store.ts
+++ habit-tracking-app/src/store/store.ts
@@ -1,5 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+// This code is setting up the Redux store for your application. 
+//The store is like a big box that holds the state (data) of your app and allows you to manage that state easily.
 
+import { configureStore } from '@reduxjs/toolkit'
+import habitReducer from '../store/habit-slice' //This slice contains logic for managing the habit state (like adding, updating, or deleting habits).
+
+//This part creates the store and assigns the habitReducer to manage the habit slice of your state.
+//The state will have a habit key.
+//The value for habit will be managed by the habitReducer (logic from your habit-slice).
 export default configureStore({
-  reducer: {}
+  reducer: {
+    habit: habitReducer 
+  }
 })
