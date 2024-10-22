eeea5bfda4230adf8df26e5eac22871e3a341551
RajithaKWG <rkrishan894@gmail.com>
2024-10-22 14:00:36 +0530

    uid more and add comments

--- habit-tracking-app/src/store/store.ts
+++ habit-tracking-app/src/store/store.ts
@@ -7,8 +7,21 @@ import habitReducer from '../store/habit-slice' //This slice contains logic for 
 //This part creates the store and assigns the habitReducer to manage the habit slice of your state.
 //The state will have a habit key.
 //The value for habit will be managed by the habitReducer (logic from your habit-slice).
-export default configureStore({
+const store = configureStore({
   reducer: {
     habit: habitReducer 
   }
 })
+
+// store.getState() is a function that returns the current state of the Redux store.
+// ReturnType is a TypeScript utility type that extracts the return type of a given function
+//will generate the type that matches the shape of your entire Redux state.
+//This allows TypeScript to know what the state looks like when you access it in your components or selectors.
+export type RootState = ReturnType<typeof store.getState>; 
+                                                                      
+
+//store.dispatch is a function used to send actions to the Redux store to update the state.
+//typeof store.dispatch will generate the correct type for the dispatch function used in your store.                                                         
+export type AppDispatch = typeof store.dispatch; //This is useful when you want to type your dispatch actions correctly inside your components or hooks.
+
+export default store;
